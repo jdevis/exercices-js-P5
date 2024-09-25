a3de77df1be92924c61055a618bb1edefed622fb
jdevis <josephine.roubertie@gmail.com>
2024-09-25 11:12:14 +0200

    exercice 9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,20 @@
 // votre code ici
-
+const errorMessages = {
+    minor: "Vous êtes mineur.",
+    major: "Vous êtes majeur.",
+    senior: "Vous êtes senior.",
+}
+function checkAge(int){
+    let afficheMessage
+    if (int <18){
+        afficheMessage = errorMessages.minor
+    }
+    if (int >= 18 && int < 65){
+        afficheMessage = errorMessages.major
+    }
+    if(int >= 65){
+        afficheMessage = errorMessages.senior
+    }
+    return afficheMessage
+}
 export default checkAge;
