42b54c33a6aadbb2f16a7ee604abc0f4a17fcb3d
jdevis <josephine.roubertie@gmail.com>
2024-09-25 12:03:02 +0200

    exercice 10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,13 @@
 //votre code ici
 
+function printNumbers(nb){
+    let tab = []
+    let message
+    for (let i = 1 ; i<nb+1; i++){
+        tab.push(i)
+    }
+    message = tab.toString().replace(/,/g,' ')
+    return message
+}
+
 export default printNumbers
